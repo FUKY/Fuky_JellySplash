01166f3216e1b61ad0b5456b96c9993539638ceb
Nguyễn Thanh Cao <caont.fuky@gmail.com>
2015-04-12 17:05:15 +0700

    first commit

---
+++ Assets/Spripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+    //thay doi mau
+    public Sprite[] sprite_Color = new Sprite[5];
+    public GameObject[] gameObjectColor = new GameObject[5];
+
+    public float x, y;
+
+    //mang hai chieu chua cac sprite
+    private int [][] list = new int[10][];
+
+
+	void Start () { 
+        RandomList();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+          
+	
+	}
+    void RandomList()
+    {
+        int randDom;
+        for (int i = 0; i < 10; i++)
+        {
+            list[i] = new int[10];
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                randDom = Random.Range(0, 5);
+                list[i][j] = randDom;
+                Debug.Log(list[i][j]);
+                Instantiate(gameObjectColor[list[i][j]], new Vector3(i * 1 + x, j * 1 + y, 0), Quaternion.identity);
+             }
+        }
+        
+
+    }
+
+}

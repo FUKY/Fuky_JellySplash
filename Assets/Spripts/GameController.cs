872339be57e85a8adca9ac9ffd5dcad14e5a68ac
Nguyễn Thanh Cao <caont.fuky@gmail.com>
2015-04-14 21:14:42 +0700

    commit properties

--- Assets/Spripts/GameController.cs
+++ Assets/Spripts/GameController.cs
@@ -1,49 +1,93 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-    //thay doi mau
-    public Sprite[] sprite_Color = new Sprite[5];
     public GameObject[] gameObjectColor = new GameObject[5];
-
     public float x, y;
 
-    //mang hai chieu chua cac sprite
-    private int [][] list = new int[10][];
+    private int[][] listInt = new int[10][];
+    private GameObject[] listGameObject = new GameObject[100];
 
+    GameObject a;
+    private List<GameObject> b = new List<GameObject>(); 
+    private List<GameObject> list = new List<GameObject>();
+    RaycastHit2D hit;
 
-	void Start () { 
+    int count = 0;
+    int posX, posY;
+    void Start()
+    {
         RandomList();
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-          
-	
-	}
+    }
+    void Update()
+    {
+        DestroyMouse();
+
+    }
+
     void RandomList()
     {
-        int randDom;
         for (int i = 0; i < 10; i++)
         {
-            list[i] = new int[10];
+            listInt[i] = new int[10];
         }
         for (int i = 0; i < 10; i++)
         {
             for (int j = 0; j < 10; j++)
             {
-                randDom = Random.Range(0, 5);
-                list[i][j] = randDom;
-                Debug.Log(list[i][j]);
-                Instantiate(gameObjectColor[list[i][j]], new Vector3(i * 1 + x, j * 1 + y, 0), Quaternion.identity);
-             }
+                listInt[i][j] = Random.Range(0, 5);
+                listGameObject[count] = gameObjectColor[listInt[i][j]];
+                Instantiate(listGameObject[count], new Vector3(i * 1 - x, j * 1 - y, 0), Quaternion.identity);
+                //a = (GameObject)Instantiate(listGameObject[count], new Vector3(i * 1 - x, j * 1 - y, 0), Quaternion.identity) as GameObject;
+                //list.Add(a);
+                count++;
+            }
         }
-        
+    }
+    void DestroyMouse()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
 
+            if (hit.collider == null)
+            {
+                return;
+            }
+            else
+            {
+                TimViTri();
+                
+                //Destroy(hit.collider.gameObject);
+                //Destroy(list[posX + posY*10]);                
+                //list[posX + posY * 10 + 1].transform.position = new Vector3(list[posX + posY * 10 + 1].transform.position.x, list[posX + posY * 10 + 1].transform.position.y - 1);
+            }
+            
+        }
+    }
+    void TimViTri()
+    {
+
+        posY = (int)(hit.transform.position.x + x );
+        posX = (int)(hit.transform.position.y + y);
+
+        Debug.Log(posX + "\t" + posY);
+
+    }
+    void ResetVitri()
+    {
+        posX = 0;
+        posY = 0;
+    }
+
+    [ContextMenu("ReloadList")]
+    public void ReloadList()
+    {
+        listGameObject[0].gameObject.active = false;
+        
     }
 
 }

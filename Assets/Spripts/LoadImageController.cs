609d40bdbe19e2ca991188d5316b29f095ce8ca4
Nguyễn Thanh Cao <caont.fuky@gmail.com>
2015-04-16 06:52:11 +0700

    Load mang Image. xoa cac cuc va, cac cuc tren roi xuong

--- Assets/Spripts/LoadImageController.cs
+++ Assets/Spripts/LoadImageController.cs
@@ -9,12 +9,15 @@ public class LoadImageController : MonoBehaviour {
     private  int index;
     public GameObject[] listGem;
     private int[][] listImage;
+
+    private GameObject[][] arrGem;
     public float x, y;
+
     private RaycastHit2D rayHit;
     private int posX, posY;
-    private List<GameObject> listGameObject = new List<GameObject>();
 
-    private List<GameObject> listDestroy = new List<GameObject>();
+    private List<GameObject> xxx = new List<GameObject>();
+
 	void Start () {
         RandomImage();
 	}
@@ -22,25 +25,25 @@ public class LoadImageController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         DestroyButtonMouse();
-        //DestroyGameObject();
+        if (Input.GetMouseButtonDown(1))
+        {
+            DestroyGameObject();
+        }
 	}
     void RandomImage()
     {
         listImage = new int[7][];
+        arrGem = new GameObject[7][];
         for(int i=0; i<7; i++)
         {
             listImage[i] = new int[8];
+            arrGem[i] = new GameObject[8];
         }
         for (int i = 0; i < 7; i++)
         {
             for (int j = 0; j < 8; j++)
-            {
-                index = Random.Range(0, 6);
-
-                GameObject a = Instantiate(listGem[index], new Vector3(i * 0.8f - x, j * 0.8f - y, 0), Quaternion.identity) as GameObject;
-                a.transform.parent = transform;
-                a.transform.localScale = Vector3.one;
-                listGameObject.Add(a);
+            {                
+                InstantiateGem(i, j);
             }
         }
     }
@@ -56,79 +59,73 @@ public class LoadImageController : MonoBehaviour {
                 return;
             }
             else
-            {
-                TimViTri();
-                int indexVitri = posX * 8 + posY;
-                //Destroy(rayHit.collider.gameObject);
-                Destroy(listGameObject[indexVitri]);              
-                Debug.Log(listGameObject[indexVitri]);
-                if (listGameObject[indexVitri] == null)
+            {  
+                if (xxx.Count == 0)
                 {
-                    Debug.Log("adsadasd");
+                    xxx.Add(rayHit.collider.gameObject);
                 }
-                
+                if(xxx.Count != 0)
+                {
+                    if (rayHit.collider.gameObject.tag == xxx[0].tag)
+                    {
+                        xxx.Add(rayHit.collider.gameObject);
+                    }
+                }
+               
+
             }
  
         }
     }
     void TimViTri()
     {
-
         posX = (int)(rayHit.transform.position.x/0.8 + x + 0.8);
         posY = (int)(rayHit.transform.position.y/0.8 + y + 0.8 );
 
         Debug.Log(posX + "\t" + posY);
 
     }
-
-    [ContextMenu("SwapObject")]
-    void SwapObject()
-    {
-        GameObject swap = new GameObject();
-        swap = listGameObject[0];
-        listGameObject[0] = listGameObject[1];
-        listGameObject[1] = swap;
-        Debug.Log(listGameObject[0]);
-        Debug.Log(swap);
-        Debug.Log(listGameObject[1]);
-        GameObject b;
-        b = Instantiate(listGameObject[0], listGameObject[1].transform.position, Quaternion.identity) as GameObject;
-        b.transform.parent = transform;
-        b.transform.localScale = Vector3.one;
-        b = Instantiate(listGameObject[1], listGameObject[0].transform.position, Quaternion.identity) as GameObject;
-        b.transform.parent = transform;
-        b.transform.localScale = Vector3.one;
-    }
-    [ContextMenu("DestroyGameObject")]
     void DestroyGameObject()
     {
-        for (int i = 0; i < listGameObject.Count; i++)
+        int countNull = 1;
+        for (int i = 0; i < 7; i++) 
         {
-            if (listGameObject[i] == null)
+            for (int j = 0; j < 8; j++) 
             {
-                listDestroy.Add(listGameObject[i]);
-                listDestroy.Add(listGameObject[i + 1]);
-                GameObject b;
 
-                //if (i != 6)
-                //{
-                //    b = Instantiate(listDestroy[1], new Vector3(listDestroy[1].transform.position.x, listDestroy[1].transform.position.y - 0.8f, 0), Quaternion.identity) as GameObject;
-                //    b.transform.parent = transform;
-                //    b.transform.localScale = Vector3.one;
-                //}
-                b = Instantiate(listDestroy[1], new Vector3(listDestroy[1].transform.position.x, listDestroy[1].transform.position.y - 0.8f, 0), Quaternion.identity) as GameObject;
-                b.transform.parent = transform;
-                b.transform.localScale = Vector3.one;
-
-                Destroy(listGameObject[i + 1]);                
-                listGameObject.RemoveAt(i + 1);
-
-                Debug.Log(i);
-                listDestroy.Clear();
-
+                if (arrGem[i][j] == null && arrGem[i][j + 1] != null) 
+                {
+                    arrGem[i][j + 1].transform.position = new Vector3(arrGem[i][j + 1].transform.position.x, arrGem[i][j + 1].transform.position.y - (float)(0.8f), arrGem[i][j + 1].transform.position.z);
+                    arrGem[i][j] = arrGem[i][j + 1];
+                    arrGem[i][j + 1] = null;
+                    if (arrGem[i][7] == null)
+                        InstantiateGem(i, 7);
+                }
+                if (arrGem[i][7] == null)
+                {
+                    InstantiateGem(i, 7);
+                }
+                      
             }
         }
-        
-        
+
+    }
+    
+    void InstantiateGem( int hang, int cot)
+    {
+        index = Random.Range(0, 6);
+        GameObject a = Instantiate(listGem[index], new Vector3(hang * 0.8f - x, cot * 0.8f - y, 0), Quaternion.identity) as GameObject;
+        arrGem[hang][cot] = a;
+        a.transform.parent = transform;
+        a.transform.localScale = Vector3.one;
+    }
+    [ContextMenu("Xoa")]
+    void Xoa()
+    {
+        for (int i = 0; i < xxx.Count; i++)
+        {
+            Destroy(xxx[i]);
+            xxx.Clear();
+        }
     }
 }

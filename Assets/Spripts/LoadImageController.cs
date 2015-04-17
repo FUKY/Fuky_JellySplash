58ab6904d18ef876daa5f05e998bf6c21fdbdd0d
Nguyễn Thanh Cao <caont.fuky@gmail.com>
2015-04-17 11:03:29 +0700

    ket noi cac cuc gan nhau va xoa khi tha tay ra kiem tra tren 3 cuc thi moi cho xoa

--- Assets/Spripts/LoadImageController.cs
+++ Assets/Spripts/LoadImageController.cs
@@ -2,50 +2,56 @@
 using System.Collections;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 
-public class LoadImageController : MonoBehaviour {
+public class LoadImageController : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler {
 
 	// Use this for initialization
     private  int index;
     public GameObject[] listGem;
+
     private int[][] listImage;
+
 
     private GameObject[][] arrGem;
     public float x, y;
+    public int soHang;
+    public int soCot;
+
+    public int score = 0;
 
     private RaycastHit2D rayHit;
-    private int posX, posY;
+    
 
     private List<GameObject> ListDelete = new List<GameObject>();
-
+   
 	void Start () {
         RandomImage();
+        //ListDelete.Contains()
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        DestroyButtonMouse();
-        if (Input.GetMouseButtonDown(1))
-        {
-            DestroyGameObject();
-        }
+        //DestroyButtonMouse();
+        DestroyGameObject();        
 	}
     void RandomImage()
     {
-        listImage = new int[7][];
-        arrGem = new GameObject[7][];
-        for(int i=0; i<7; i++)
+        listImage = new int[soCot][];
+        arrGem = new GameObject[soCot][];
+        for(int i=0; i<soCot; i++)
         {
-            listImage[i] = new int[8];
-            arrGem[i] = new GameObject[8];
+            listImage[i] = new int[soHang];
+            arrGem[i] = new GameObject[soHang];
         }
-        for (int i = 0; i < 7; i++)
+        for (int i = 0; i < soCot; i++)
         {
-            for (int j = 0; j < 8; j++)
+            for (int j = 0; j < soHang; j++)
             {                
                 InstantiateGem(i, j);
             }
         }
+
     }
 
     void DestroyButtonMouse()
@@ -77,17 +83,21 @@ public class LoadImageController : MonoBehaviour {
  
         }
     }
-    void TimViTri()
+    int TimViTriX(GameObject vitrClick)
     {
-        posX = (int)(rayHit.transform.position.x/0.8 + x + 0.8);
-        posY = (int)(rayHit.transform.position.y/0.8 + y + 0.8 );
-
-        Debug.Log(posX + "\t" + posY);
-
+        int posX;
+        posX = (int)(vitrClick.transform.position.x / 0.8 + x + 0.8);
+        
+        return posX;
+    }
+    int TimViTriY(GameObject vitrClick)
+    {
+        int posY;
+        posY = (int)(vitrClick.transform.position.y / 0.8 + y + 0.8);
+        return posY;
     }
     void DestroyGameObject()
     {
-        int countNull = 1;
         for (int i = 0; i < 7; i++) 
         {
             for (int j = 0; j < 8; j++) 
@@ -113,21 +123,91 @@ public class LoadImageController : MonoBehaviour {
     
     void InstantiateGem( int hang, int cot)
     {
-        index = Random.Range(0, 6);
+        index = Random.Range(0, 5);
         GameObject a = Instantiate(listGem[index], new Vector3(hang * 0.8f - x, cot * 0.8f - y, 0), Quaternion.identity) as GameObject;
         arrGem[hang][cot] = a;
         a.transform.parent = transform;
         a.transform.localScale = Vector3.one;
     }
-    [ContextMenu("Xoa")]
+
     void Xoa()
     {
-        Debug.Log(ListDelete.Count);
-        for (int i = 0; i < ListDelete.Count; i++)
+
+        if (ListDelete.Count >= 3)
         {
-            Destroy(ListDelete[i]);
-            
+            for (int i = 0; i < ListDelete.Count; i++)
+            {
+                Destroy(ListDelete[i]);
+                score += 100;
+            }            
         }
         ListDelete.Clear();
     }
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        rayHit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        if (rayHit.collider == null)
+        {
+            return;
+        }
+        else 
+        {
+            if (ListDelete.Count == 0)
+            {
+                ListDelete.Add(rayHit.collider.gameObject);
+            }
+        }
+        
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Xoa();
+
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        rayHit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        if (rayHit.collider == null)
+        {
+            return;
+        }
+        else
+        {
+            if (ListDelete[0] == null)
+            {
+                return;
+            }
+            if (rayHit.collider.gameObject.tag == ListDelete[0].tag && KiemTraKhoangCach(rayHit.collider.gameObject,ListDelete[ListDelete.Count - 1]) <= 1.5f)
+            {
+                if (!ListDelete.Contains(rayHit.collider.gameObject))
+                    ListDelete.Add(rayHit.collider.gameObject);
+            }
+        }
+        Debug.Log(ListDelete.Count);
+        
+    }
+
+    float KiemTraKhoangCach(GameObject a, GameObject b)
+    {
+        float khoangCach = Vector3.Distance(a.transform.position, b.transform.position);
+        return khoangCach;
+    }
+
+    void KiemTraViTri(GameObject vitriClick)
+    {
+        int posX = TimViTriX(vitriClick);
+        int posY = TimViTriY(vitriClick);
+
+        for (int i = posX - 1; i <= posX + 1; i++)
+        {
+            for (int j = posY - 1; j <= posY + 1; j++)
+            { }
+        }
+
+    }
+
 }
